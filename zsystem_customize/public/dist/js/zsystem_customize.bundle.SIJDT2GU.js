7e32cb21a258e6fcb3dae745feab59936c938dee
kathiresanvkm <kathiresan@mazeworkssolutions.com>
2025-12-20 12:53:48 +0530

    modification Custom_contackquickform and add field in quotation

--- zsystem_customize/public/dist/js/zsystem_customize.bundle.SIJDT2GU.js
+++ zsystem_customize/public/dist/js/zsystem_customize.bundle.SIJDT2GU.js
@@ -572,6 +572,70 @@
     }
   };
   frappe.ui.form.CustomerQuickEntryForm = ZsystemCustomerQuickEntryForm;
+  var ZsystemGSTQuickEntryForm = class extends GSTQuickEntryForm {
+    get_address_fields() {
+      const fields = super.get_address_fields();
+      for (const field of fields) {
+        const fieldname = field.fieldname === "_pincode" ? "pincode" : field.fieldname;
+        if (!field.label && fieldname) {
+          field.label = frappe.meta.get_label("Address", fieldname);
+        }
+      }
+      return fields;
+    }
+    render_dialog() {
+      this.mandatory = [
+        ...this.get_gstin_field(),
+        ...this.mandatory,
+        ...this.get_contact_fields(),
+        ...this.get_address_fields()
+      ];
+      if (this.doctype === "Customer") {
+        this.mandatory.push({
+          label: __("Customer POS ID"),
+          fieldname: "customer_pos_id",
+          fieldtype: "Data",
+          hidden: 1
+        });
+      }
+      super.render_dialog();
+    }
+    get_contact_fields() {
+      return [
+        {
+          label: __("Primary Contact Details"),
+          fieldname: "primary_contact_section",
+          fieldtype: "Section Break",
+          collapsible: 0
+        },
+        {
+          label: __("Email ID"),
+          fieldname: "_email_id",
+          fieldtype: "Data",
+          options: "Email",
+          reqd: 1
+        },
+        {
+          fieldtype: "Column Break"
+        },
+        {
+          label: __("Mobile Number"),
+          fieldname: "_mobile_no",
+          fieldtype: "Data",
+          reqd: 1
+        }
+      ];
+    }
+    update_doc() {
+      const doc = super.update_doc();
+      doc._address_line1 = doc.address_line1;
+      delete doc.address_line1;
+      doc.email_id = doc._email_id;
+      doc.mobile_no = doc._mobile_no;
+      return doc;
+    }
+  };
+  frappe.ui.form.CustomerQuickEntryForm = ZsystemGSTQuickEntryForm;
 
   // ../zsystem_customize/zsystem_customize/public/js/override_quotation.js
   frappe.provide("zsystem_customize.selling");
@@ -750,4 +814,4 @@
     }
   });
 })();
-//# sourceMappingURL=zsystem_customize.bundle.JNWL75G4.js.map
+//# sourceMappingURL=zsystem_customize.bundle.SIJDT2GU.js.map
--- zsystem_customize/public/js/override_contactquickform.js
+++ zsystem_customize/public/js/override_contactquickform.js
@@ -106,3 +106,83 @@ class ZsystemCustomerQuickEntryForm
 
 // ✅ Proper override (after frappe loads)
 frappe.ui.form.CustomerQuickEntryForm = ZsystemCustomerQuickEntryForm;
+
+//update Indian Complaince
+class ZsystemGSTQuickEntryForm extends GSTQuickEntryForm {
+    get_address_fields() {
+        const fields = super.get_address_fields();
+
+        for (const field of fields) {
+            const fieldname =
+                field.fieldname === "_pincode" ? "pincode" : field.fieldname;
+
+            if (!field.label && fieldname) {
+                field.label = frappe.meta.get_label("Address", fieldname);
+            }
+        }
+
+        return fields;
+    }
+
+    render_dialog() {
+        this.mandatory = [
+            ...this.get_gstin_field(),
+            ...this.mandatory,
+            ...this.get_contact_fields(),
+            ...this.get_address_fields(),
+        ];
+
+        if (this.doctype === "Customer") {
+            this.mandatory.push({
+                label: __("Customer POS ID"),
+                fieldname: "customer_pos_id",
+                fieldtype: "Data",
+                hidden: 1,
+            });
+        }
+
+        super.render_dialog();
+    }
+
+    get_contact_fields() {
+        return [
+            {
+                label: __("Primary Contact Details"),
+                fieldname: "primary_contact_section",
+                fieldtype: "Section Break",
+                collapsible: 0,
+            },
+            {
+                label: __("Email ID"),
+                fieldname: "_email_id",
+                fieldtype: "Data",
+                options: "Email",
+                reqd: 1,
+            },
+            {
+                fieldtype: "Column Break",
+            },
+            {
+                label: __("Mobile Number"),
+                fieldname: "_mobile_no",
+                fieldtype: "Data",
+                reqd: 1,
+            },
+        ];
+    }
+
+    update_doc() {
+        const doc = super.update_doc();
+
+        // to prevent clash with ERPNext
+        doc._address_line1 = doc.address_line1;
+        delete doc.address_line1;
+
+        // these fields were suffixed with _ to prevent them from being read only
+        doc.email_id = doc._email_id;
+        doc.mobile_no = doc._mobile_no;
+
+        return doc;
+    }
+}
+frappe.ui.form.CustomerQuickEntryForm = ZsystemGSTQuickEntryForm;
