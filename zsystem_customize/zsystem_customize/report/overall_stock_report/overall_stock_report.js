3d5a2528a8b2018c5870bc939caa217ccb1f4027
vignesh-maze <Vignesh@mazeworkssolutions.com>
2026-05-25 15:45:44 +0530

    overall_stock_report

--- zsystem_customize/zsystem_customize/report/overall_stock_report/overall_stock_report.js
+++ zsystem_customize/zsystem_customize/report/overall_stock_report/overall_stock_report.js
@@ -2,7 +2,89 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Overall Stock Report"] = {
-	"filters": [
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1,
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			reqd: 1,
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+		},
+		{
+			fieldname: "warehouse",
+			label: __("Warehouses"),
+			fieldtype: "MultiSelectList",
+			options: "Warehouse",
+			get_data: function (txt) {
+				const company = frappe.query_report.get_filter_value("company");
 
-	]
+				return frappe.db.get_link_options("Warehouse", txt, {
+					company: company,
+				});
+			},
+		},
+		{
+			fieldname: "item_code",
+			label: __("Items"),
+			fieldtype: "Link",
+			options: "Item",
+		},
+		{
+			fieldname: "item_group",
+			label: __("Item Group"),
+			fieldtype: "Link",
+			options: "Item Group",
+		},
+		{
+			fieldname: "brand",
+			label: __("Brand"),
+			fieldtype: "Link",
+			options: "Brand",
+		},
+		{
+			fieldname: "voucher_no",
+			label: __("Voucher #"),
+			fieldtype: "Data",
+		},
+		{
+			fieldname: "voucher_type",
+			label: __("Voucher Type"),
+			fieldtype: "Select",
+			options: "\nStock Entry\nStock Reconciliation\nSales Invoice\nPurchase Invoice\nDelivery Note\nPurchase Receipt",
+		},
+	],
+	formatter: function (value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (column.fieldname == "out_qty" && data && data.out_qty < 0) {
+			value = "<span style='color:red'>" + value + "</span>";
+		} else if (column.fieldname == "in_qty" && data && data.in_qty > 0) {
+			value = "<span style='color:green'>" + value + "</span>";
+		}
+
+		return value;
+	},
+
+	// onload: function (report) {
+	// 	report.page.add_inner_button(__("View Stock Balance"), function () {
+	// 		var filters = report.get_values();
+	// 		frappe.set_route("query-report", "Stock Balance", filters);
+	// 	});
+	// },
 };
+
+erpnext.utils.add_inventory_dimensions("Stock Ledger", 10);
